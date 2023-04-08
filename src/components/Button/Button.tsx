b873ebb1502be72ce9d601a866a9db9ad1167c9a
avkotau <kotov.alexander1@mail.ru>
2023-04-08 15:36:18 +0200

    last commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FilterValuesType } from "../../App";
+
+type ButtonType = {
+    name: FilterValuesType | '+'
+    callBackButton: () => void
+}
+
+const Button = (props: ButtonType) => {
+    const { name, callBackButton} = props
+
+    const onClickHandler = () => {
+        callBackButton()
+    }
+
+    return (
+        <button onClick={onClickHandler}>{name}</button>
+    );
+};
+
+export default Button;
